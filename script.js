9731cf370f99134b11f350a1a7733f3c6f5085b7
Akku97kkr <Akku97kkr@hotmail.com>
2021-05-16 14:47:46 +0000

    Changes made by Akash

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var years = new Date().getFullYear()+parseInt(years);
     
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+years+"\<br\>"
+
 }
-        
+    
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("Rate").innerText=rateval;
+}
+
+function checkdata()
+{
+    var principal = document.getElementById("principal");
+    
+    //check if principal field is empty
+
+    if(principal.value == "0"){
+        alert("Please enter the Amount");
+        principal.focus();
+        return false;
+    }
+
+    if(principal.value == /^-\d+$/){
+    alert("Please enter the Amount");
+    principal.focus();
+    return false;
+    }
+    return true;
+}
